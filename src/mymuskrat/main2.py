92fecc21663b071d72e922f67186d8eeab6b5a0e
Taylor Monacelli <taylormonacelli@gmail.com>
2024-02-29 07:41:50 -0800

    better docs

--- src/mymuskrat/main2.py
+++ src/mymuskrat/main2.py
@@ -4,20 +4,17 @@ import jinja2
 
 TEMPLATES_DIR = "templates"
 
+
 def load_template(template_name):
     TEMPLATES_PATH = pathlib.Path(__file__).resolve().parent / TEMPLATES_DIR
     path = TEMPLATES_PATH / template_name
-    print(f"Loading template from {path}")
     with open(path, "r") as file:
         return file.read()
 
-def render_template(template_content):
-    print(f"__file__: {__file__}")
-    print(f"__name__: {__name__}")
-    package_name = __name__.split(".")[0]
-    print(f"package_name: {package_name}")
-    loader = jinja2.PackageLoader(package_name, package_path=TEMPLATES_DIR)
-    env = jinja2.Environment(loader=loader)
 
+def render_template(template_content):
+    package_name = __name__.split(".")[0]
+    loader = jinja2.PackageLoader(package_name, TEMPLATES_DIR)
+    env = jinja2.Environment(loader=loader)
     template = env.from_string(template_content)
     return template.render()
