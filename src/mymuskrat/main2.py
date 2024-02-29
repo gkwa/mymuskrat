58db9b8fd75340ef8c90e3a6200c125b7dcb6ec1
Taylor Monacelli <taylormonacelli@gmail.com>
2024-02-29 06:51:07 -0800

    working

--- src/mymuskrat/main2.py
+++ src/mymuskrat/main2.py
@@ -1,11 +1,8 @@
 import pathlib
-from jinja2 import Environment, PackageLoader, select_autoescape
 
 import jinja2
 
 TEMPLATES_DIR = "templates"
-
-
 
 def load_template(template_name):
     TEMPLATES_PATH = pathlib.Path(__file__).resolve().parent / TEMPLATES_DIR
@@ -14,15 +11,13 @@ def load_template(template_name):
     with open(path, "r") as file:
         return file.read()
 
-
 def render_template(template_content):
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATES_DIR))
-
-
-    env = Environment(
-        loader=PackageLoader("mymuskrat"),
-        autoescape=select_autoescape()
-    )
+    print(f"__file__: {__file__}")
+    print(f"__name__: {__name__}")
+    package_name = __name__.split(".")[0]
+    print(f"package_name: {package_name}")
+    loader = jinja2.PackageLoader(package_name, package_path=TEMPLATES_DIR)
+    env = jinja2.Environment(loader=loader)
 
     template = env.from_string(template_content)
     return template.render()
