f57fa449f1db826116cfff7c2cc22b710fe2d072
Taylor Monacelli <taylormonacelli@gmail.com>
2024-02-29 06:12:20 -0800

    refactor

--- src/mymuskrat/__init__.py
+++ src/mymuskrat/__init__.py
@@ -4,7 +4,7 @@ from . import main2
 
 
 def main() -> int:
-    template_content = main2.load_template("main.go.j2")
+    template_content = main2.load_template("main2.go.j2")
     out = main2.render_template(template_content)
     sys.stdout.write(out)
     return 0
--- src/mymuskrat/main2.py
+++ src/mymuskrat/main2.py
@@ -2,13 +2,17 @@ import pathlib
 
 import jinja2
 
+TEMPLATES_DIR = "templates"
+
+
 def load_template(template_name):
-    TEMPLATES_PATH = pathlib.Path(__file__).resolve().parent / "templates"
+    TEMPLATES_PATH = pathlib.Path(__file__).resolve().parent / TEMPLATES_DIR
     path = TEMPLATES_PATH / template_name
     with open(path, "r") as file:
         return file.read()
 
+
 def render_template(template_content):
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATES_DIR))
     template = env.from_string(template_content)
     return template.render()
