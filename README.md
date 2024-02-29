8f55612ee45796257778ccfd0a57d98a1ea57b74
Taylor Monacelli <taylormonacelli@gmail.com>
2024-02-29 08:01:31 -0800

    refactor

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # mymuskrat
 
+
 ## Learning jinja2 template inheritance
 
-https://jinja.palletsprojects.com/en/latest/templates/#template-inheritance
+https://jinja.palletsprojects.com/en/3.0.x/api/#basics
+
+
+## output
+
+```
+(mymuskrat) [mtm@taylors-MacBook-Pro-2:mymuskrat(master)]$ hello
+I come from base.j2
+I come from extend.j2
+(mymuskrat) [mtm@taylors-MacBook-Pro-2:mymuskrat(master)]$
+```
--- src/mymuskrat/main2.py
+++ src/mymuskrat/main2.py
@@ -2,11 +2,9 @@ import pathlib
 
 import jinja2
 
-TEMPLATES_DIR = "templates"
-
 
 def load_template(template_name):
-    TEMPLATES_PATH = pathlib.Path(__file__).resolve().parent / TEMPLATES_DIR
+    TEMPLATES_PATH = pathlib.Path(__file__).resolve().parent / "templates"
     path = TEMPLATES_PATH / template_name
     with open(path, "r") as file:
         return file.read()
@@ -14,7 +12,7 @@ def load_template(template_name):
 
 def render_template(template_content):
     package_name = __name__.split(".")[0]
-    loader = jinja2.PackageLoader(package_name, TEMPLATES_DIR)
+    loader = jinja2.PackageLoader(package_name)
     env = jinja2.Environment(loader=loader)
     template = env.from_string(template_content)
     return template.render()
